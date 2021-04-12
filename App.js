c5271d3fe2fc85ee00166a824841f2d1c6bfe998
Aniket Mahajan <007aniketmahajan@gmail.com>
2021-04-12 21:23:34 +0530

    Init App v0.0.2

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import {Container, Content, Root, Text, Toast} from 'native-base';
+import {Container, Content, Root, Text, Toast, View} from 'native-base';
 import NavHeader from "./components/NavHeader/NavHeader";
 import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import {StyleSheet} from "react-native";
+import {AntDesign, Ionicons} from '@expo/vector-icons';
+import {Alert, StyleSheet} from "react-native";
 import Formtext from "./components/Formtext/Formtext";
 import TodoList from "./components/TodoList/TodoList";
 import uuid from 'react-uuid';
@@ -48,6 +48,35 @@ export default class ButtonThemeExample extends Component {
             todolist: updatelist
         });
     }
+    clearAll = () => {
+        Alert.alert(
+            "Clear All Task",
+            "Are you sure you want to clear all task?",
+            [
+                {
+                    text: "Cancel",
+                    style: "cancel",
+                },
+                {
+                    text: "Ok",
+                    onPress: this.ClearAllAction,
+                    style: "ok",
+                },
+            ],
+            {
+                cancelable: true
+            }
+        );
+    }
+    ClearAllAction = () => {
+        this.setState({
+            todolist: []
+        });
+        Toast.show({
+            text: "All task removed.",
+            duration: 3000
+        });
+    }
     async componentDidMount() {
         await Font.loadAsync({
             Roboto: require('native-base/Fonts/Roboto.ttf'),
@@ -72,6 +101,12 @@ export default class ButtonThemeExample extends Component {
               fontStyle: "italic",
               fontWeight: "bold",
               fontSize:18
+          },
+          icon_delete:{
+              flexDirection: 'row',
+              alignItems:'flex-end',
+              justifyContent:'flex-end',
+              marginRight:14
           }
       })
         if (!this.state.isReady) {
@@ -86,6 +121,9 @@ export default class ButtonThemeExample extends Component {
                     <Text style={topmargin.leftmar}>
                         Awesome To do list:
                     </Text>
+                    <View style={topmargin.icon_delete} >
+                        <AntDesign onPress={this.clearAll} name="delete" size={24} color="black" />
+                    </View>
                     <TodoList update_task={this.UpdateTask} list_data={this.state.todolist} />
                 </Content>
             </Container>
